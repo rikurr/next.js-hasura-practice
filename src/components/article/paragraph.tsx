31656ccea8c4fa61db853e0f888210b5c6488000
oya riku <>
2020-09-22 10:11:51 +0900

    リファクタリング

---
+++ src/components/article/paragraph.tsx
@@ -0,0 +1,7 @@
+mport React from 'react'
+
+import styles from './index.module.css'
+
+export const Paragraph: React.FC<{ p: string }> = ({ p }) => {
+  return <div className={styles.paragraph}>{p}</div>
+}
