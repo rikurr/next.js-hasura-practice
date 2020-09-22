1c49f50d6396b53be85eb04346e21cd8d6ca49b6
oya riku <>
2020-09-22 10:04:10 +0900

    見た目を整える

--- src/pages/[userId]/[articleId]/index.tsx
+++ src/pages/[userId]/[articleId]/index.tsx
@@ -1,7 +1,11 @@
 import { useRouter } from 'next/router'
 import { NextPage } from 'next'
+import Error from 'next/error'
 
 import { useGetArticleQuery } from '@/generated/graphql'
+
+import styles from './index.module.css'
+import { Article } from '@/components/article'
 
 const ArticlePage: NextPage = () => {
   const router = useRouter()
@@ -19,7 +23,27 @@ const ArticlePage: NextPage = () => {
   if (error) {
     return <p>{error.toString()}</p>
   }
-  return <div>{JSON.stringify(data)}</div>
+  if (!data || !data.articles_by_pk) {
+    return <Error statusCode={404} />
+  }
+  const { user, subject, content } = data.articles_by_pk
+  return (
+    <div className={styles.contentContainer}>
+      <h1 className={styles.subject}>{subject}</h1>
+      <div className={styles.userContainer}>
+        <div>
+          <img className={styles.userIcon} src="/profile.png" />
+        </div>
+        <div>
+          <div className={styles.userId}>{user.displayId}</div>
+          <div className={styles.username}>{user.displayName}</div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <Article content={content} />
+      </div>
+    </div>
+  )
 }
 
 export default ArticlePage
