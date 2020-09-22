ba44a753a334b16ce7f1643c3b491131a7ac80ac
oya riku <>
2020-09-22 13:48:26 +0900

    日付TDD-4

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -8,4 +8,10 @@ describe('formatDate', () => {
       isNew: true,
     })
   })
+  test('一週間経過', () => {
+    expect(formatDate(new Date('2020-09-14 09:21:32'), now)).toEqual({
+      datetime: '2020/09/14 09:21',
+      isNew: false,
+    })
+  })
 })
